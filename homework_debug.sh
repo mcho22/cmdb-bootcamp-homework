1aef92219786e5cff3e73ce250de98ae801ed791
mcho22 <mcho22@jhu.edu>
2014-09-02 19:37:12 -0400

    small changes

--- homework_debug.sh
+++ homework_debug.sh
@@ -14,7 +14,7 @@ CORES=4
 
 for i in [:893-916]
 do
-  fastqc $FASTQ_DIR/$SAMPLE_PREFIX$i\.fastq.gz -o $OUTPUT_DIR
+  fastqc $FASTQ_DIR/$SAMPLE_PREFIX$i\.fastq.gz -o $OUTPUT_DIR/$i
   tophat -p $CORES -G $OUTPUT_DIR/$ANNOTATION -o $RESULT_DIR$i $GENOME_DIR/dmel-all-chromosome-r5.57 $FASTQ_DIR/$SAMPLE_PREFIX$i\.fastq.gz
   cufflinks -p $CORES -G $OUTPUT_DIR/$ANNOTATION -o $RESULT_DIR$i $RESULT_DIR$i/accepted_hits.bam
 done
