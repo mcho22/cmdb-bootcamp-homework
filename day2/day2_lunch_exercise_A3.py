e510d0845a40fb74a5b3479c9e6a623b51c451fb
mcho22 <mcho22@jhu.edu>
2014-09-03 14:21:53 -0400

    small changes due to practical problems

--- day2/day2_lunch_exercise_A3.py
+++ day2/day2_lunch_exercise_A3.py
@@ -6,18 +6,22 @@ f = open ( sam_location )
 num_lines = 0
 
 for i, line in enumerate (f):
-    
-    fields = line.rstrip("\r\n").split("\t")
-    phred_raw = str(fields[10])
-    phred_list = list(phred_raw)
-    read_length = len(str(fields[9]))
-    phred_trs = 0
-    
-    for char in phred_list:
-        phred_trs = phred_trs + ord(char) - 33
-        phred_ave = float(phred_trs) / float(read_length) 
+
+    if i <= 1000000:
+        fields = line.rstrip("\r\n").split("\t")
+        phred_raw = str(fields[10])
+        phred_list = list(phred_raw)
+        read_length = len(str(fields[9]))
+        phred_trs = 0
+        
+        for char in phred_list:
+            phred_trs = phred_trs + ord(char) - 33
+            phred_ave = float(phred_trs) / float(read_length) 
             
-    if phred_ave > 30:
-        num_lines += 1
+        if phred_ave > 30:
+            num_lines += 1
+
+    if i > 1000000:
+        break
                 
 print "Number of lines with average phred score > 30 = " + str(num_lines)
